39eba14a055c5090859763a5bd4319742ce4cc6e
Wojciech <duttybwooy@gmail.com>
2023-01-06 19:27:52 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+{
+const tasks = [];
+
+const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+};
+
+const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+};
+
+const addNewTask = (newTaskContent) => {
+    tasks.push({ content: newTaskContent });
+    render();
+};
+
+}
