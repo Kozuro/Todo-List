6bda0b01fa2976fd4ed52b4b57cc6c866fe883bb
Wojciech Rybacki <duttybwooy@gmail.com>
2023-01-17 18:05:25 +0100

    fixed problem in line 77

--- js/script.js
+++ js/script.js
@@ -74,7 +74,7 @@
               <button class="buttons__button js-toggleHideDoneTasks">
                 ${hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
               </button>
-              <button class="buttons__button js-markAllDone" ${ tasks.every(({ done }) => done ? " disabled" : "")}>
+              <button class="buttons__button js-markAllDone" ${ tasks.every(({ done }) => done) ? " disabled" : ""}>
                 Ukończ wszystkie
               </button>
             `;
